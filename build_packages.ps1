b89682c16bb1b34ab945bb9a78fb5553ebdaa86d
Jerker <jerker@ecredence.com>
2025-08-04 10:15:45 +0800

    remove temp files

--- build_packages.ps1
+++ build_packages.ps1
@@ -3,7 +3,7 @@ $version="2.1.0"
 $package="dothttp"
 cd src/${package}
 dotnet pack -p:PackageVersion=$version
-nuget push bin/Release/${package}.${version}.nupkg -Source https://api.nuget.org/v3/index.json
+nuget push bin/Release/DotHttpTestCLI.${version}.nupkg -Source https://api.nuget.org/v3/index.json
 cd ../..
 
 $package="DotHttpTest"
